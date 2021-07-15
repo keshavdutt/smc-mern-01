be22d32743029d76b6a90bc7669de28e102e3fb0
mohanbarman <mohanbarman9711@gmail.com>
2021-07-15 16:49:49 +0530

    Adding files

---
+++ src/quickFind.ts
@@ -0,0 +1,11 @@
+/**
+ * Solve quick find
+ * Explaination:
+ *  - Generate 0-n nodes.
+ *  - Create union method to join two nodes for example: 2, 5
+ *          1        2
+ *          \        \
+ *           3 - 6    5
+ *  - Create connected method. input: node1, node2 -> true if nodes are connected, false if not
+ */
+export class QuickFind {}
---
+++ src/quickUnion.ts
@@ -0,0 +1,11 @@
+/**
+ * Solve quick union
+ * Explaareination:
+ *  - Generate 0-n nodes.
+ *  - Create @union method to join two nodes for example: 2, 5
+ *          1        2
+ *        / \        \
+ *       6   3        5
+ *  - Create @connected method. input: node1, node2 -> true if nodes are connected, false if not
+ */
+export class QuickUnion {}
